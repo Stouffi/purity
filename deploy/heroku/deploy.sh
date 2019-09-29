7a6eda33d922c54626035542b728307949f4a7f7
Stouffi <steff.leb@orange.fr>
2019-09-29 17:17:03 +0200

    WIP deploy

--- deploy/heroku/deploy.sh
+++ deploy/heroku/deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+# pwd
 heroku git:remote -a stouffi
-git co -b deploy
+git stash
+bower i
 pulp build
+cp -f deploy/heroku/gitignore .gitignore
+cp -f deploy/heroku/Procfile Procfile
+cp -f deploy/heroku/package.json package.json
+cp deploy/heroku/index.js output
+git add -A
+git commit -m "deploy"
+git push heroku master
+git reset --hard HEAD^
+git stash pop
